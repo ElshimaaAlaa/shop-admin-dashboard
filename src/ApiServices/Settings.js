1b02d12e26691c752881224a5d8aa42508cbb027
AlshimaaAlaa <shymaaalaam23@gmail.com>
2025-05-14 03:02:09 +0300

    some refactors

--- src/ApiServices/Settings.js
+++ src/ApiServices/Settings.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 const API_BASE_URL = "https://";
-const live_shop_domain = localStorage.getItem("live_shop_domain");
-const role = localStorage.getItem("role");
+// const live_shop_domain = localStorage.getItem("live_shop_domain");
+// const role = localStorage.getItem("role");
 export const settings = async () => {
   try {
     const response = await axios({
-      url: `${API_BASE_URL}${live_shop_domain}/api/${role}/settings`,
+      url: `${API_BASE_URL}demo.vrtex.duckdns.org/api/shop/settings`,
       method: "GET",
       headers: {
         "Content-Type": "application/json",
